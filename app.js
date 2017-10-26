fc06599b3cbc7134e422c068297ffd9059cfc5ff
Alexader <tyxj58@gmail.com>
2017-10-26 20:56:27 +0800

    add some pages for route

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var home = require('./routes/hoem');
-var users = require('./routes/users');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
+var settings = require('./setting');
+
+var index = require('./routes/index');
+var users = require('./routes/users')
 var login = require('./routes/login');
 var logout = require('./routes/logout');
 var register = require('./routes/register');
@@ -24,22 +29,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride());
+// app.use(session({
+//   secret: settings.cookieSecret,
+//   store: new MongoStore({
+//     db: settings.db
+//   })
+// }));
 
-app.use('/',  home);
+app.use('/', index);
 app.use('/users', users);
 app.use('./login', login);
 app.use('./register', register);
 app.use('./logout', logout);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
