49f18e71b86f8bd738f453d51202f13c60c5cab5
Alexader <tyxj58@gmail.com>
2017-10-27 17:36:31 +0800

    design for sign up page

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('home', { title: 'Express' });
 });
 
-exports.user = function(req, res) {
-};
-exports.reg = function(req, res) {
-};
-exports.doReg = function(req, res) {
-};
-exports.login = function(req, res) {
-};
-exports.doLogin = function(req, res) {
-};
-exports.logout = function(req, res) {
-};
 module.exports = router;
