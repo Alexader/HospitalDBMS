7478d6d03b9356d95c000522ff8f0ad7b3540b49
Alexader <tyxj58@gmail.com>
2017-11-26 11:41:14 +0800

    finish search by age

--- routes/search.js
+++ routes/search.js
@@ -8,24 +8,32 @@ var connection = require("../Mysql/db");
 router.post('/search', function (req, res, next) {
     req.key = req.body.key;
     req.searchOption = req.body.searchOption;
-    var attribute;
+    //different search mode
     switch(req.searchOption) {
       case 'age':
-        attribute = 'Birthday';
+        req.attribute = 'Birthday';
+        //assume you got a string comtains number 
+        var now = new Date();
+        req.Min = new Date(now.getFullYear() - req.key, 0, 1).toJSON();
+        req.Max = new Date(now.getFullYear() - req.key, 11, 31).toJSON();
+        req.sql = ' between '+ connection.escape(req.Min) + ' and ' + connection.escape(req.Max);
         break;
       case 'name':
-        attribute = 'Name';
+        req.attribute = 'Name';
+        req.sql = ' = '+ connection.escape(req.key);
         break;
       case 'id':
-        attribute = 'id';
+        req.attribute = 'id';
+        req.sql = ' = '+ connection.escape(req.key);
         break;
       default:
-        attribute = 'Name';
+        req.attribute = 'Name';
+        req.sql = ' = '+ connection.escape(req.key);
     }
     if(req.key.length>0) {
       //find related patient
       var sql = 'select Name,Sex,date_format(Birthday, "%Y-%m-%d") AS birth,Contact,Sympton from patient where '+ 
-        attribute + ' = '+ connection.escape(req.key);
+        req.attribute + req.sql;
       connection.query(sql, function(err, results, fields) {
         if(err) return console.log(err);
         //check if it is empty result 
@@ -41,7 +49,7 @@ router.post('/search', function (req, res, next) {
   }, function(req, res, next) {
     //find related doctor
     var sql = 'select Name,Sex,date_format(Birthday, "%Y-%m-%d") AS birth,Contact,ProffesionalTitle,CareerYear from doctor where '+ 
-      attribute + ' = '+ connection.escape(req.key);
+      req.attribute + req.sql;
     connection.query(sql, function(err, results, fields) {
       if(err) return console.log(err);
       //check if it is empty result 
@@ -55,21 +63,32 @@ router.post('/search', function (req, res, next) {
     })
   }, function(req, res, next) {
     //find related hospital
-    connection.query('select * from hospital where '+ attribute+ ' = '+ connection.escape(req.key), function(err, results, fields) {
-      if(err) return console.log(err);
-      //check if it is empty result
-      if(results.length>0) {
-        req.hospital = results;
-        console.log(results);
-      } else {
-        req.hospital = "没有找到";
-      }
+    //hospital don't have attribute `Birthday`
+    if(req.attribute==='Birthday') {
+      req.hospital = '没有找到';
       res.render('searchPage', {
         patient: req.patient,
         hospital: req.hospital,
         doctor: req.doctor,
+      });
+    } else{
+      var sql = 'select * from hospital where '+ req.attribute+ req.sql;
+      connection.query(sql, function(err, results, fields) {
+        if(err) return console.log(err);
+        //check if it is empty result
+        if(results.length>0) {
+          req.hospital = results;
+          console.log(results);
+        } else {
+          req.hospital = "没有找到";
+        }
+        res.render('searchPage', {
+          patient: req.patient,
+          hospital: req.hospital,
+          doctor: req.doctor,
+        })
       })
-    })
+    }
     });
 
 module.exports = router;
